b1d58c5e761b6c10adb6af82884de432850ac21a
harufumi555 <120348465+harufumi555@users.noreply.github.com>
2023-06-28 12:11:28 +0900

    VectorBubbleX追加

--- bubble.cpp
+++ bubble.cpp
@@ -1,16 +1,34 @@
 #include"bubble.h"
 #include"Dxlib.h"
+#include"Common.h"
 void bubble::GenerationBubble() const{
 
 	DrawBox(bubbleX, bubbleY, bubbleX+10, bubbleY +10, GetColor(255, 0, 0), TRUE);
-}
+	DrawFormatString(10, 100, C_WHITE, "%d", bubbleX);
+}	
 void bubble::UpdateBubble()
 {
 	FPSCount++;
 
-	if (FPSCount % 60 == 0) {
-		bubbleY -= 10;
-		bubbleX -= 10;
-		
+
+	bubbleY--;
+	bubbleX += VectorBubbleX;
+
+	if (bubbleX - 50 > bubbleX) {
+		VectorBubbleX*=-1;
 	}
+	else if (bubbleX + 50 < bubbleX) {
+		VectorBubbleX*=-1;
+	}
+	
+
+	/*if (FPSCount % 60 == 0) {
+		bubbleY -= 10;
+		if (FPSCount % 30 == 0) {
+			bubbleX -= 10;
+		}
+		else {
+			bubbleX +=10;
+		}
+	}*/
 }
--- bubble.h
+++ bubble.h
@@ -4,6 +4,7 @@ private:
 	int bubbleX=320;// シャボン玉のX軸
 	int bubbleY=480;// シャボン玉のY軸
 	int FPSCount=0; // フレームカウント
+	int VectorBubbleX=-1;
 public:
 	void GenerationBubble() const;
 	void UpdateBubble();
