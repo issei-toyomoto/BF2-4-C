aa2d28c553292b806a1d892fdf2b7a9b06ac3294
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-06-28 12:13:44 +0900

    enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,1 +1,22 @@
 #include "Enemy.h"
+
+Enemy::Enemy()
+{
+	EnemyX = 0;
+	EnemyY = 0;
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::Update()
+{
+
+}
+
+void Enemy::Draw() const
+{
+
+}
--- Enemy.h
+++ Enemy.h
@@ -2,7 +2,16 @@
 
 class Enemy
 {
-public:
+private:
+	int EnemyX;
+	int EnemyY;
 
+public:
+	Enemy();   // コンストラクタ
+	~Enemy();  // デストラクタ
+	//描画以外の更新を実装する
+	void Update();
+	//描画の更新を実装する
+	void Draw() const;
 };
 
