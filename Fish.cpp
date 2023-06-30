89f715bcc896f159bf9998d1a1a04191b123e8c0
ikehara-rin <120348343+ikehara-rin@users.noreply.github.com>
2023-06-30 11:30:22 +0900

    魚関数（仮）追加

--- Fish.cpp
+++ Fish.cpp
@@ -1,3 +1,19 @@
 #include"Fish.h"
 #include"DxLib.h"
 #include<math.h>
+
+void Fish::CreateFish() {
+	DrawBox;
+}
+
+void Fish::HitBoxArea() {
+
+}
+
+void Fish::DrawFish() {
+
+}
+
+void Fish::MoveFish() {
+
+}
