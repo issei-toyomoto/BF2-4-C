e885badad41868dea03348d6479e13bff3b884d3
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2023-06-30 21:30:34 +0900

    プレイヤー画像追加、画像配列の要素のマクロ定義

--- Player.cpp
+++ Player.cpp
@@ -5,21 +5,43 @@
 
 #define DEBUG
 
+Player::Player() 
+{
+	LoadDivGraph("image/Player/Player_animation_d.png", 32, 8, 4, 64, 64, PlayerImg);//‰æ‘œ“Ç‚Ýž‚Ý
+	FPSCnt = 0;
+}
+
+
 void Player::Update() /***•`‰æˆÈŠO***/
 {
+	FPSCnt++;
+
 	InputKey::Update();
 	GetJoypadAnalogInput(&XStick, &YStick, DX_INPUT_PAD1);
 
 	InputKey::GetJoyStickX(XStick);
 	InputKey::GetJoyStickY(YStick);
+
+	if (FPSCnt > 60) {
+		FPSCnt = 0;
+	}
 }
 
 void Player::Draw() const /***•`‰æ***/
 {
-
+	DrawGraph(100, 100, PlayerImg[P_Img_Thunder_1], TRUE);
 
 #ifdef DEBUG
-	DrawFormatString(10, 10, C_WHITE, "X:%d Y:%d",XStick,YStick);
-	DrawBox(XStick, YStick, XStick + 10, YStick + 10, C_RED, TRUE);
+	DrawFormatString(480, 10, C_WHITE, "FPS:%d", FPSCnt);
 #endif // DEBUG
+}
+
+void Player::UpdatePlayerMove() 
+{
+	
+}
+
+void Player::DrawPlayerMove()const 
+{
+
 }
--- Player.h
+++ Player.h
@@ -1,23 +1,70 @@
 #pragma once
+//**********画像変数の配列要素番号**********//
+//待機状態（風船２個）
+#define P_Img_Wait_Ballon_2_0 0
+#define P_Img_Wait_Ballon_2_1 1
+#define P_Img_Wait_Ballon_2_2 2
+//待機状態（風船１個）
+#define P_Img_Wait_Ballon_1_0 3
+#define P_Img_Wait_Ballon_1_1 4
+#define P_Img_Wait_Ballon_1_2 5
+//走る（風船２個）
+#define P_Img_Run_Ballon_2_0 8
+#define P_Img_Run_Ballon_2_1 9
+#define P_Img_Run_Ballon_2_2 10
+#define P_Img_Run_Ballon_2_3 11
+//走る（風船１個）
+#define P_Img_Run_Ballon_1_0 12
+#define P_Img_Run_Ballon_1_1 13
+#define P_Img_Run_Ballon_1_2 14
+#define P_Img_Run_Ballon_1_3 15
+//浮く（風船２個）
+#define P_Img_Float_Ballon_2_0 16
+#define P_Img_Float_Ballon_2_1 17
+#define P_Img_Float_Ballon_2_2 18
+#define P_Img_Float_Ballon_2_3 19
+#define P_Img_Float_Ballon_2_4 20
+//浮く（風船１個）
+#define P_Img_Float_Ballon_1_0 24
+#define P_Img_Float_Ballon_1_1 25
+#define P_Img_Float_Ballon_1_2 26
+#define P_Img_Float_Ballon_1_3 27
+#define P_Img_Float_Ballon_1_4 28
+//落下
+#define P_Img_Drop_0 21
+#define P_Img_Drop_1 22
+#define P_Img_Drop_2 23
+//雷に当たる
+#define P_Img_Thunder_0 29
+#define P_Img_Thunder_1 30
+//*******************************************//
 
 class Player
 {
 private:
-	int PlayerX;//プレイヤーX座標
-	int PlayerY;//プレイヤーY座標
-	int VectorX;//ベクトルX
-	int VectorY;//ベクトルY
+	int FPSCnt;
+
+	int PlayerImg[32];	//プレイヤー画像
+	int PlayerX;		//プレイヤーX座標
+	int PlayerY;		//プレイヤーY座標
+	int VectorX;		//ベクトルX
+	int VectorY;		//ベクトルY
 	
-	int XStick;
-	int YStick;
+	int XStick;//ステック上下
+	int YStick;//ステック左右
 
 public:
 	//コンストラクタ
-	Player() {};
+	Player();
 	//デストラクタ
 	~Player() {};
 	//描画以外の更新を実装する
 	void Update();
 	//描画の更新を実装する
 	void Draw() const;
+
+	//移動（地面＆空中）
+	void UpdatePlayerMove();
+	//移動（地面＆空中）
+	void DrawPlayerMove() const;
 };
