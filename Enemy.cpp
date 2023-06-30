9fe0a59a3caa09fd67ada090b63fbf767aa87384
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-06-30 14:14:25 +0900

    enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -5,7 +5,9 @@ Enemy::Enemy()
 {
 	EnemyX = 300;
 	EnemyY = 250;
+	EnemyState = 0;
 	EnemyFlg = 0;
+	EnemyLife = 0;
 	StartFlg = 0;
 }
 
@@ -18,24 +20,13 @@ void Enemy::Update()
 {
 	if (StartFlg == 0)
 	{
-		for (int i = 0; i < 7; i++)
-		{
-			EnemyFlg++;
-
-			if (EnemyFlg == 2)
-			{
-				EnemyFlg = 0;
-			}
-		}
-
-		WaitTimer(200);
+		EnemyStart();
 	}
-	
 }
 
 void Enemy::Draw() const
 {
-	if (EnemyFlg == 0)
+	if (EnemyFlg == 0 &&  EnemyState == 0)
 	{
 		DrawBox(EnemyX, EnemyY, EnemyX + 20, EnemyY + 20, 0xffffff, TRUE);
 	}
@@ -43,4 +34,38 @@ void Enemy::Draw() const
 	{
 		DrawBox(EnemyX, EnemyY, EnemyX + 20, EnemyY + 20, 0xff0000, TRUE);
 	}
+	if (EnemyFlg == 2)
+	{
+		DrawBox(EnemyX, EnemyY, EnemyX + 20, EnemyY + 20, 0x00ff00, TRUE);
+	}
+	if (EnemyFlg == 3)
+	{
+		DrawBox(EnemyX, EnemyY, EnemyX + 20, EnemyY + 20, 0x0000ff, TRUE);
+	}
+	if (EnemyFlg == 4)
+	{
+		DrawBox(EnemyX, EnemyY, EnemyX + 20, EnemyY + 20, 0xffffff, TRUE);
+	}
+	if (EnemyFlg == 5)
+	{
+		DrawBox(EnemyX, EnemyY, EnemyX + 20, EnemyY + 20, 0xff0000, TRUE);
+	}
+	if (EnemyFlg == 6)
+	{
+		DrawBox(EnemyX, EnemyY, EnemyX + 20, EnemyY + 20, 0x00ff00, TRUE);
+	}
+}
+
+void Enemy::EnemyStart()
+{
+
+	EnemyFlg++;
+
+	if (EnemyFlg == 7)
+	{
+		EnemyFlg = 0;
+		StartFlg = 1;
+	}
+
+	WaitTimer(300);
 }
--- Enemy.h
+++ Enemy.h
@@ -5,12 +5,17 @@ class Enemy
 private:
 	int EnemyX;
 	int EnemyY;
+	int EnemyState;
+	int EnemyLife;
 	int EnemyFlg;
 	int StartFlg;
 
 public:
 	Enemy();   // コンストラクタ
 	~Enemy();  // デストラクタ
+
+	void EnemyStart();
+
 	//描画以外の更新を実装する
 	void Update();
 	//描画の更新を実装する
