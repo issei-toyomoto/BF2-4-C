e9679c1f1eb14fbd72b2c5769b6f12a36fed5471
ikehara-rin <120348343+ikehara-rin@users.noreply.github.com>
2023-07-04 14:14:05 +0900

    sakana

--- Fish.cpp
+++ Fish.cpp
@@ -2,18 +2,68 @@
 #include"DxLib.h"
 #include<math.h>
 
+//コンストラクタ
+Fish::Fish() {
+	/*FishImage = LoadImage();*/
+	WaitTime=0;
+	f_PosX=0;
+	f_PosY=0;
+	Count = 0;
+	Second = 0;
+	FishRand = 0;
+	FishFlg = FALSE;
+	f_Count = 60;
+}
+
+//魚生成(Draw)
 void Fish::CreateFish() {
-	DrawBox;
+	if (f_Count < 60) {
+		DrawBox;	//魚画像
+		/*DrawGraph()*/
+	}
 }
 
-void Fish::HitBoxArea() {
+//出現エリア(Update)
+void Fish::FishArea() {
 
+	////出現エリア判定
+	//if (playerY > 610 || enemyY > 610) {
+	//	while (playerY < 610 && enemyY<610) {
+	//		Count++;
+	//		Second = Count / 60;
+	//		if (Second >= 3 && FishFlg == FALSE) {
+	//			FishRand = GetRand(99);
+	//			Count = 0;
+	//			Second = 0;
+	//			if (FishRand <= 29) {
+	//				FishFlg = TRUE;
+	//			}
+	//		}
+	//		if (FishFlg == TRUE) {
+	//			MoveFish();
+	//		}
+	//	}
+	//	if (FishFlg == TRUE&&f_Count<60) {
+	//		MoveFish();
+	//	}
+	//}
 }
 
-void Fish::DrawFish() {
-
-}
-
+//魚移動
 void Fish::MoveFish() {
-
+	/*
+	if (enemyY > 610) {
+		f_PosX = enemyX;
+		f_PosY = enemyY - f_Count;
+		f_Count = f_Count - 1;
+	}
+	else if (playerY > 610) {
+		f_PosX = playerX;
+		f_PosY = playerY - f_Count;
+		f_Count = f_Count - 1;
+	}
+	else if (playerY < 610 && enemyY < 610) {
+		f_PosY = f_PosY + f_Count;
+		f_Count = f_Count + 1;
+	}*/
 }
--- Fish.h
+++ Fish.h
@@ -3,10 +3,16 @@ class Fish
 {
 private:
 	int WaitTime;
-	int PosX, PosY;
+	int Count;
+	int f_Count;
+	int Second;
+	int FishRand;
+	int f_PosX, f_PosY;
+	bool FishFlg;
+	/*int FishImage;*/
 public:
+	Fish();		//コンストラクタ
 	void CreateFish();	//魚生成
-	void HitBoxArea();	//出現エリア判定
-	void DrawFish();	//魚描画
+	void FishArea();	//出現エリア判定
 	void MoveFish();	//魚移動
 };
