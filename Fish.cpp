9cd4aa8f58aa152f8edc952dc0dcd14abce48bbf
ikehara-rin <120348343+ikehara-rin@users.noreply.github.com>
2023-07-11 11:24:09 +0900

    fish

--- Fish.cpp
+++ Fish.cpp
@@ -4,7 +4,7 @@
 
 //コンストラクタ
 Fish::Fish() {
-	/*FishImage = LoadImage();*/
+	LoadDivGraph("image/Enemy/Enemy_FishAnimation.png",10,6,2,64,64,FishImage);
 	WaitTime=0;
 	f_PosX=0;
 	f_PosY=0;
@@ -12,38 +12,69 @@ Fish::Fish() {
 	Second = 0;
 	FishRand = 0;
 	FishFlg = FALSE;
-	f_Count = 60;
+	f_Count = 120;
 }
 
 //魚生成(Draw)
-void Fish::CreateFish() {
-	//if (f_Count < 60) {
-	//	DrawBox;	//魚画像
-	//	/*DrawGraph()*/
+void Fish::Draw() {
+	//if (f_Count < 120) {
+	//		//魚画像(アニメーション)
+	//		switch(f_Count/17){
+	//		case 6:
+	//			DrawGraph(f_PosX,f_PosY,FishImage[0],TRUE);
+	//			break;
+	//		case 5:
+	//			DrawGraph(f_PosX,f_PosY,FishImage[1],TRUE);
+	//			break;
+	//		case 4:
+	//			DrawGraph(f_PosX,f_PosY,FishImage[2],TRUE);
+	//			break;
+	//		case 3:
+	//			switch(){
+	//			case Player:
+	//				DrawGraph(f_PosX,f_PosY,FishImage[6],TRUE);
+	//				break;
+	//			case Enemy:	//敵によって画像変更
+	//				DrawGraph(f_PosX,f_PosY,FishImage[EnemyFlg],TRUE);
+	//				break;
+	//			}
+	//		 case 2:
+	//			DrawGraph(f_PosX,f_PosY,FishImage[4],TRUE);
+	//			break;
+	//		case 1:
+	//			DrawGraph(f_PosX,f_PosY,FishImage[5],TRUE);
+	//			break;
+	//		case 0:
+	//			DrawGraph(f_PosX,f_PosY,FishImage[6],TRUE);
+	//			break;
 	//}
 }
 
 //出現エリア(Update)
-void Fish::FishArea() {
+void Fish::Update() {
 
 	////出現エリア判定
-	//if (playerY > 610 || enemyY > 610) {
-	//	while (playerY < 610 && enemyY<610) {
+	//if (playerY > 600 || enemyY > 600) {
+	//	while (playerY < 600 && enemyY<600) {
 	//		Count++;
 	//		Second = Count / 60;
+	//		//三秒経過＆魚がいない
 	//		if (Second >= 3 && FishFlg == FALSE) {
 	//			FishRand = GetRand(99);
 	//			Count = 0;
 	//			Second = 0;
+	//			//魚出現
 	//			if (FishRand <= 29) {
 	//				FishFlg = TRUE;
 	//			}
 	//		}
+	//		//魚移動
 	//		if (FishFlg == TRUE) {
 	//			MoveFish();
 	//		}
 	//	}
-	//	if (FishFlg == TRUE&&f_Count<60) {
+	// //魚出現時＆２秒経過前
+	//	if (FishFlg == TRUE&&f_Count<120) {
 	//		MoveFish();
 	//	}
 	//}
@@ -51,19 +82,28 @@ void Fish::FishArea() {
 
 //魚移動
 void Fish::MoveFish() {
-	/*
-	if (enemyY > 610) {
+
+	//攻撃対象が敵
+	/*if (enemyY > 600) {
 		f_PosX = enemyX;
 		f_PosY = enemyY - f_Count;
 		f_Count = f_Count - 1;
-	}
-	else if (playerY > 610) {
-		f_PosX = playerX;
-		f_PosY = playerY - f_Count;
-		f_Count = f_Count - 1;
-	}
-	else if (playerY < 610 && enemyY < 610) {
-		f_PosY = f_PosY + f_Count;
-		f_Count = f_Count + 1;
+		if(f_Count == 0){
+			EnemyLife = 0;
+		}
 	}*/
+	//攻撃対象がプレイヤー
+	//if (playerY > 600) {
+		//f_PosX = playerX;
+		//f_PosY = playerY - f_Count;
+		//f_Count = f_Count - 1;
+		/*if(f_Count == 0){
+			PlayerLife = 0;
+		}*/
+	//}
+	//攻撃対象が範囲から出たとき
+	//else if (playerY < 600 /*&& enemyY < 600*/) {
+		//f_PosY = f_PosY + f_Count;
+		//f_Count = f_Count + 1;
+	//}
 }
