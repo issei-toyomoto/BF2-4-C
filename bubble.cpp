06ac166bb2a8a57e875a6fe9680df3c55dc9ce75
harufumi555 <120348465+harufumi555@users.noreply.github.com>
2023-07-13 09:52:37 +0900

    p

--- bubble.cpp
+++ bubble.cpp
@@ -5,7 +5,7 @@ bubble::bubble() {
 	LoadDivGraph("images/Stage_BubbleAnimation.png", 4, 4, 1, 64, 64, BubbleImg);//âÊëúì«Ç›çûÇ›
 }
 void bubble::GenerationBubble() const{
-	DrawGraph((int)bubbleX, (int)bubbleY, BubbleImg[3], TRUE);
+	DrawGraph((int)bubbleX, (int)bubbleY, BubbleImg[0], TRUE);
 	//DrawBox((int)bubbleX, (int)bubbleY, (int)bubbleX+10, (int)bubbleY +10, GetColor(255, 0, 0), TRUE);
 	//DrawFormatString(10, 100, C_WHITE, "%d", bubbleX);
 }	
