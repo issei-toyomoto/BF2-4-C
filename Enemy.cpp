b668cdb55115bdd7458b2c56a924dd8142cf6f19
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-07-19 11:11:00 +0900

    enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -7,17 +7,13 @@
 // コンストラクタ
 Enemy::Enemy()
 {
-	enemy.x = ENEMY_X;    // 敵のX座標
-	enemy.y = ENEMY_Y;    // 敵のY座標
-	enemy.state = 0;      // 敵の状態(0:ピンク 1:緑 2:黄色)
-	enemy.life = 2;       // 敵のHP
-	enemy.flg = 0;        // 敵の画像番号用
-	enemy.speed = 0.4f;   // 敵の移動速度
-	StartFlg = 0;         // スタート状態か判定する用
-	FPScnt = 0;           // FPSカウント
-	i = 0;                // スタート時、敵のモーション管理用
-	Px = Player::PlayerX;
-	Py = Player::PlayerY;
+	EnemyInit();
+
+	StartFlg = 0;   // スタート状態か判定する用
+	FPScnt = 0;     // FPSカウント
+	StartMotion = 0;          // スタート時、敵のモーション管理用
+	Px = 0;         // プレイヤーのX座標
+	Py = 0;         // プレイヤーのY座標
 
 	LoadDivGraph("image/Enemy/Enemy_P_Animation.png", 18, 6, 3, 64, 64, EnemyImg);  //画像読み込み
 }
@@ -31,24 +27,30 @@ Enemy::~Enemy()
 // 描画以外の更新を実装する
 void Enemy::Update()
 {
+	
 	FPScnt++;
 
+	// プレイヤーの座標取得
 	Px = Player::PlayerX;
 	Py = Player::PlayerY;
 
-	if (StartFlg == 0 && i < 4)
+	if (StartFlg == 0 && StartMotion < 4)
 	{
 		StartMove();
 	}
-	else if(StartFlg != 0 && enemy.state == 0)
+	else if(StartFlg != 0)
 	{
 		EnemyMove();
 	}
 	else
 	{
-		enemy.x = ENEMY_X;
-		enemy.y = ENEMY_Y;
-		enemy.state = 0;
+		for (int j = 0; j < ENEMY_MAX; j++)
+		{
+			enemy[j].x = ENEMY_X;
+			enemy[j].y = ENEMY_Y;
+			enemy[j].state = 0;
+		}
+
 		StartFlg = 0;
 	}
 
@@ -56,7 +58,7 @@ void Enemy::Update()
 	if (FPScnt > 60) 
 	{
 		FPScnt = 0;
-		i++;
+		StartMotion++;
 	}
 }
 
@@ -64,222 +66,242 @@ void Enemy::Update()
 void Enemy::Draw() const
 {
 #ifdef _DEBUG
-	DrawFormatString(50, 50, 0xffffff, "EnX:%f EnY:%f", enemy.x, enemy.y);
-	DrawFormatString(50, 70, 0xffffff, "Enflg:%d", enemy.flg);
+	DrawFormatString(50, 50, 0xffffff, "EnX:%f EnY:%f", enemy[2].x, enemy[2].y);
+	DrawFormatString(50, 70, 0xffffff, "Enflg:%d", enemy[0].flg);
 	DrawFormatString(50, 100, 0xffffff, "Px:%f", Px);
 	DrawFormatString(50, 130, 0xffffff, "Py:%f", Py);
 
-	// 敵の当たり判定表示
-	if (enemy.state == 0)
+	for (int j = 0; j < ENEMY_MAX; j++)
 	{
-		if (StartFlg == 0)
+		// 敵の当たり判定表示
+		if (enemy[j].state == 0)
 		{
-			// スタートモーション時
-			DrawBox((int)enemy.x, (int)enemy.y + 25, (int)enemy.x + 50, (int)enemy.y + 64, 0xffffff, FALSE);
-			DrawBox((int)enemy.x + 85, (int)enemy.y + 25, (int)enemy.x + 50 + 85, (int)enemy.y + 64, 0xffffff, FALSE);
-			DrawBox((int)enemy.x + 170, (int)enemy.y + 25, (int)enemy.x + 50 + 170, (int)enemy.y + 64, 0xffffff, FALSE);
-		}
-		else
-		{
-			// スタート以外
-			DrawBox((int)enemy.x + 10, (int)enemy.y + 12, (int)enemy.x + 55, (int)enemy.y + 65, 0xffffff, FALSE);
-			/*DrawBox(EnemyX + 95, EnemyY + 10, EnemyX + 55 + 85, EnemyY + 64, 0xffffff, FALSE);
-			DrawBox(EnemyX + 180, EnemyY + 10, EnemyX + 55 + 170, EnemyY + 64, 0xffffff, FALSE);*/
-
+			if (StartFlg == 0)
+			{
+				// スタートモーション時
+				DrawBox((int)enemy[j].x, (int)enemy[j].y + 25, (int)enemy[j].x + 50, (int)enemy[j].y + 64, 0xffffff, FALSE);
+			}
+			else
+			{
+				// スタート以外
+				DrawBox((int)enemy[j].x + 10, (int)enemy[j].y + 12, (int)enemy[j].x + 55, (int)enemy[j].y + 65, 0xffffff, FALSE);
+				
+			}
 		}
 	}
 
 #endif // DEBUG
 
-	// 敵画像の表示
-	if (enemy.life != 0)
+	for (int j = 0; j < ENEMY_MAX; j++)
 	{
-		if (StartFlg == 0)
+		// 敵画像の表示
+		if (enemy[j].life != 0)
 		{
-			// スタートモーション時
-			DrawGraph((int)enemy.x, (int)enemy.y, EnemyImg[enemy.flg], TRUE);
-			DrawGraph((int)enemy.x + 85, (int)enemy.y, EnemyImg[enemy.flg], TRUE);
-			DrawGraph((int)enemy.x + 170, (int)enemy.y, EnemyImg[enemy.flg], TRUE);
-		}
-		else
-		{
-			// スタート以外
-			// 画面内
-			if (Px >= enemy.x + 10)
+			if (StartFlg == 0)
 			{
-				DrawTurnGraph((int)enemy.x, (int)enemy.y, EnemyImg[enemy.flg], TRUE);
+				// スタートモーション時
+				DrawGraph((int)enemy[j].x, (int)enemy[j].y, EnemyImg[enemy[j].flg], TRUE);
 			}
 			else
 			{
-				DrawGraph((int)enemy.x, (int)enemy.y, EnemyImg[enemy.flg], TRUE);
-			}
-			
-			// 画面外
-			if (enemy.x < 0)
-			{
-				DrawGraph(640 + (int)enemy.x, (int)enemy.y, EnemyImg[enemy.flg], TRUE);
-			}
-			else if (enemy.x > 640 - 64)
-			{
-				DrawGraph((int)enemy.x - 640, (int)enemy.y, EnemyImg[enemy.flg], TRUE);
+				// スタート以外
+				// 画面内
+				if (Px >= enemy[j].x + 10)
+				{
+					DrawTurnGraph((int)enemy[j].x, (int)enemy[j].y, EnemyImg[enemy[j].flg], TRUE);
+				}
+				else
+				{
+					DrawGraph((int)enemy[j].x, (int)enemy[j].y, EnemyImg[enemy[j].flg], TRUE);
+				}
+
+				// 画面外
+				if (enemy[j].x < 0)
+				{
+					DrawGraph(640 + (int)enemy[j].x, (int)enemy[j].y, EnemyImg[enemy[j].flg], TRUE);
+				}
+				else if (enemy[j].x > 640 - 64)
+				{
+					DrawGraph((int)enemy[j].x - 640, (int)enemy[j].y, EnemyImg[enemy[j].flg], TRUE);
+				}
 			}
 		}
+	}
+}
+
+// 敵の初期化処理
+void Enemy::EnemyInit()
+{
+	for (int j = 0; j < ENEMY_MAX; j++)
+	{
+		enemy[j].x = ENEMY_X + 85 * j;
+		enemy[j].y = ENEMY_Y;
 	}
 }
 
 // 敵の移動処理
 void Enemy::EnemyMove()
 {
-
-	// 敵のX座標範囲
-	if (enemy.x <= 0)
+	for (int j = 0; j < ENEMY_MAX; j++)
 	{
-		enemy.x = 640;
+		// 敵のX座標範囲
+		if (enemy[j].x <= 0)
+		{
+			enemy[j].x = 640;
+		}
+		else if (enemy[j].x > 640)
+		{
+			enemy[j].x = 0;
+		}
+
+		// 敵のY座標範囲
+		if (enemy[j].y <= -20)
+		{
+			enemy[j].y = enemy[j].y * 0.8f;
+		}
+		else if (enemy[j].y > 356)
+		{
+			enemy[j].speed = enemy[j].speed;
+			enemy[j].y = 356;
+		}
+
+
+		// プレイヤーが敵より右にいるときは右に移動
+		if (Px >= enemy[j].x)
+		{
+			// X座標加算
+			enemy[j].x += enemy[j].speed;
+
+		}
+		else
+		{
+			// プレイヤーが敵より左にいるときは左に移動
+			// X座標減算
+			enemy[j].x -= enemy[j].speed;
+
+		}
+
+
+		// プレイヤーが敵より上にいるときは浮上する
+		if (Py < enemy[j].y)
+		{
+			EnemyUp();
+
+			// Y座標減算
+			enemy[j].y -= enemy[j].speed;
+		}
+		else
+		{
+			EnemyDown();
+
+			// Y座標減算
+			enemy[j].y += enemy[j].speed;
+
+		}
 	}
-	else if (enemy.x > 640)
-	{
-		enemy.x = 0;
-	}
-
-	// 敵のY座標範囲
-	if (enemy.y <= -15)
-	{
-		enemy.y = enemy.y * 0.8f;
-	}
-	else if (enemy.y > 356)
-	{
-		enemy.speed = enemy.speed;
-		enemy.y = 356;
-	}
-
-
-	// プレイヤーが敵より右にいるときは右に移動
-	if (Px >= enemy.x)
-	{
-		// X座標加算
-		enemy.x += enemy.speed;
-
-	}
-	else
-	{
-		// プレイヤーが敵より左にいるときは左に移動
-		// X座標減算
-		enemy.x -= enemy.speed;
-
-	}
-
-
-	// プレイヤーが敵より上にいるときは浮上する
-	if (Py < enemy.y)
-	{
-		EnemyUp();
-
-		// Y座標減算
-		enemy.y -= enemy.speed;
-	}
-	else 
-	{
-		EnemyDown();
-
-		// Y座標減算
-		enemy.y += enemy.speed;
-
-	}
-
 	
 }
 
 // 敵のスタート処理
 void Enemy::StartMove()
 {
-		if (i == 0)
+	for (int j = 0; j < ENEMY_MAX; j++)
+	{
+		if (StartMotion == 0)
 		{
 			if (FPScnt > 0 && FPScnt < 15 || FPScnt > 31 && FPScnt < 45)
 			{
-				enemy.flg = 0;
+				enemy[j].flg = 0;
 			}
 			else
 			{
-				enemy.flg = 1;
+				enemy[j].flg = 1;
 			}
 		}
-		else if(i == 1)
+		else if (StartMotion == 1)
 		{
 			if (FPScnt > 0 && FPScnt < 15 || FPScnt > 31 && FPScnt < 45)
 			{
-				enemy.flg = 2;
+				enemy[j].flg = 2;
 			}
 			else
 			{
-				enemy.flg = 3;
+				enemy[j].flg = 3;
 			}
 		}
-		else if(i == 2)
+		else if (StartMotion == 2)
 		{
 			if (FPScnt > 0 && FPScnt < 15 || FPScnt > 31 && FPScnt < 45)
 			{
-				enemy.flg = 4;
+				enemy[j].flg = 4;
 			}
 			else
 			{
-				enemy.flg = 5;
+				enemy[j].flg = 5;
 			}
 		}
-		else if (i == 3)
+		else if (StartMotion == 3)
 		{
 			if (FPScnt > 0 && FPScnt < 15 || FPScnt > 31 && FPScnt < 45)
 			{
-				enemy.flg = 6;
+				enemy[j].flg = 6;
 			}
 			else
 			{
-				enemy.flg = 7;
+				enemy[j].flg = 7;
 				StartFlg = 1;
-				i = 0;
+				StartMotion = 0;
 			}
 		}
+	}
 }
 
 // 敵の浮上モーション処理
 void Enemy::EnemyUp()
 {
-	// パタパタ手を動かすモーション
-	if (FPScnt > 0 && FPScnt < 5 || FPScnt > 11 && FPScnt < 15 || FPScnt > 21 && FPScnt < 25)
+	for (int j = 0; j < ENEMY_MAX; j++)
 	{
-		enemy.flg = 8;
-	}
-	else if (FPScnt > 31 && FPScnt < 35 || FPScnt > 41 && FPScnt < 45 || FPScnt > 51 && FPScnt < 55)
-	{
-		enemy.flg = 8;
-	}
-	else
-	{
-		enemy.flg = 9;
+		// パタパタ手を動かすモーション
+		if (FPScnt > 0 && FPScnt < 5 || FPScnt > 11 && FPScnt < 15 || FPScnt > 21 && FPScnt < 25)
+		{
+			enemy[j].flg = 8;
+		}
+		else if (FPScnt > 31 && FPScnt < 35 || FPScnt > 41 && FPScnt < 45 || FPScnt > 51 && FPScnt < 55)
+		{
+			enemy[j].flg = 8;
+		}
+		else
+		{
+			enemy[j].flg = 9;
+		}
 	}
 }
 
 // 敵の降下モーション処理
 void Enemy::EnemyDown()
 {
-	// 降下モーション
-	if (FPScnt > 6 && FPScnt < 10 || FPScnt > 26 && FPScnt < 30 || FPScnt > 46 && FPScnt < 50)
+	for (int j = 0; j < ENEMY_MAX; j++)
 	{
-		enemy.flg = 11;
-	}
-	else if (FPScnt > 16 && FPScnt < 20 || FPScnt > 36 && FPScnt < 40 || FPScnt > 56 && FPScnt < 60)
-	{
-		enemy.flg = 12;
-	}
-	else
-	{
-		enemy.flg = 10;
+		// 降下モーション
+		if (FPScnt > 0 && FPScnt < 20)
+		{
+			enemy[j].flg = 11;
+		}
+		else if (FPScnt > 21 && FPScnt < 40)
+		{
+			enemy[j].flg = 12;
+		}
+		else
+		{
+			enemy[j].flg = 10;
 
-		// 10 11 10 12 10 
+			// 10 11 10 12 10 
+		}
 	}
 }
 
 // 敵とステージの当たり判定処理
-void Enemy::EnemyCollision()
+void Enemy::StageCollision()
 {
 
 }
--- Enemy.h
+++ Enemy.h
@@ -2,17 +2,7 @@
 
 #define ENEMY_X 200
 #define ENEMY_Y 185
-
-// 敵のデータ(構造体)
-struct EnemyData
-{
-	float x;     // 敵のX座標
-	float y;     // 敵のY座標
-	int state;   // 敵の状態(0:ピンク 1:緑 2:黄色)
-	int life;    // 敵のHP
-	int flg;     // 敵の画像番号用
-	float speed; // 敵の移動速度
-};
+#define ENEMY_MAX 3
 
 class Enemy
 {
@@ -20,21 +10,33 @@ private:
 	int StartFlg;      // スタート状態か判定する用
 	int EnemyImg[24];  // 敵の画像格納用
 	int FPScnt;        // FPSカウント
-	int i;             // スタート時、敵のモーション管理用
-	EnemyData enemy;   // 敵のデータ(変数宣言)
+	int StartMotion;   // スタート時、敵のモーション管理用
+	float Px, Py;      // プレイヤーのX座標、Y座標
 
-	float Px,Py;         // プレイヤーのX座標、Y座標
+	// 敵のデータ(構造体)
+	struct ENEMY
+	{
+		float x = 0;     // 敵のX座標
+		float y = 0;     // 敵のY座標
+		int state = 0;   // 敵の状態(0:ピンク 1:緑 2:黄色)
+		int life = 2;    // 敵のHP
+		int flg = 0;     // 敵の画像番号用
+		float speed = 0.4f; // 敵の移動速度
+	};
+
+	struct ENEMY enemy[ENEMY_MAX];   // 敵のデータ(変数宣言)
 
 public:
 	Enemy();   // コンストラクタ
 	~Enemy();  // デストラクタ
 
+	void EnemyInit();       // 敵の初期化処理
 	void EnemyMove();       // 敵の移動処理
 	void StartMove();       // 敵のスタート処理
 	void EnemyUp();         // 敵の浮上モーション処理
 	void EnemyDown();       // 敵の降下モーション処理
-	void EnemyCollision();  // 敵とステージの当たり判定処理
-
+	void StageCollision();  // 敵とステージの当たり判定
+	
 	//描画以外の更新を実装する
 	void Update();
 	//描画の更新を実装する
