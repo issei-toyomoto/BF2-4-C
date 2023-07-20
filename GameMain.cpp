6e57a05f41214654b545f70119ca39c8fd543df7
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2023-07-20 10:58:45 +0900

    当たり判定修正

--- GameMain.cpp
+++ GameMain.cpp
@@ -23,7 +23,7 @@ AbstractScene* GameMain::Update()
 {
 	player.Update(gStageState);
 	BUBBLE.UpdateBubble();
-	enemy.Update();
+	//enemy.Update();
 	fish.Update();
 	thunder.MoveThunder();
 
@@ -55,7 +55,7 @@ void GameMain::Draw()const
 	thunder.Draw();
 	BUBBLE.GenerationBubble();
 	player.Draw();
-	enemy.Draw();
+	//enemy.Draw();
 	fish.Draw();
 
 	/*ステージ遷移*/
