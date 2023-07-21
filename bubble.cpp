73ad368fa2b2f33c53e6e5b0fd8b6e6940dfd779
harufumi555 <120348465+harufumi555@users.noreply.github.com>
2023-07-21 12:04:07 +0900

    bubble

--- bubble.cpp
+++ bubble.cpp
@@ -7,6 +7,7 @@ bubble::bubble() {
 	LoadDivGraph("images/Stage_BubbleAnimation.png", 4, 4, 1, 64, 64, BubbleImg);//画像読み込み
 	px = 0;
 	py = 0;
+	BubleAnim = 0;
 }
 
 
@@ -14,7 +15,7 @@ bubble::bubble() {
 void bubble::Update()
 {
 	FPSCount++;
-
+	// プレイヤーのX座標、Y座標
 	px = Player::PlayerX;
 	py = Player::PlayerY;
 
@@ -31,19 +32,49 @@ void bubble::Update()
 	if (FPSCount == 60) {
 		FPSCount = 0;
 	}
+	if (px == bubbleX && py == bubbleY) {
+		BubleAnim = 1;
+	}
+
 
 }
 
 void bubble::Draw() const
 {
-	DrawGraph((int)bubbleX, (int)bubbleY, BubbleImg[0], TRUE);
-
-	if (Player::PlayerX, Player::PlayerY == bubbleX, bubbleY) {
-		DrawGraph((int)bubbleX, (int)bubbleY, BubbleImg[3], TRUE);
+	
+	switch (BubleAnim)
+	{
+	case 0:
+		DrawGraph((int)bubbleX, (int)bubbleY, BubbleImg[0], TRUE);
+		DrawBox((int)bubbleX + 15, (int)bubbleY + 15, (int)bubbleX + 50, (int)bubbleY + 50, C_RED, FALSE);
+		break;
+	case 1:
+		/*DrawGraph((int)bubbleX, (int)bubbleY, BubbleImg[3], TRUE);*/
+		/*DrawGraph((int)bubbleX, (int)bubbleY, BubbleImg[0], TRUE);*/
+		DrawBox((int)bubbleX + 15, (int)bubbleY + 15, (int)bubbleX + 50, (int)bubbleY + 50, C_WHITE, FALSE);
+		break;
+	default:
+		break;
 	}
 	
-	//DrawBox((int)bubbleX, (int)bubbleY, (int)bubbleX+10, (int)bubbleY +10, GetColor(255, 0, 0), TRUE);
+	
+	//DrawBox((int)bubbleXY, (int)bubbleY, (int)bubbleX+10, (int)bubbleY +10, GetColor(255, 0, 0), TRUE);
 	//DrawFormatString(10, 100, C_WHITE, "%d", bubbleX);
+}
+// シャボン玉の当たり判定
+void bubble::BubleCollision()
+
+{
+	//シャボン玉の矩形の座標
+	int BubleX1, BubleY1, BubleX2, BubleY2; // 左上座標(x1,x2) 右上座標(x2,y2)
+	BubleX1 = (int)bubbleX + 15;
+	BubleY1 = (int)bubbleY + 15;
+	BubleX2 = (int)bubbleX + 50;
+	BubleY2 = (int)bubbleY + 50;
+
+	if (BubleX1 <= Player::PlayerX + 18 && BubleY1 <=  Player::PlayerY +14  && BubleX2 >= Player::PlayerX + 40 && BubleY2 >=  Player::PlayerY + 64 ) {
+		BubleAnim = 1;
+	}
 }
 
 
