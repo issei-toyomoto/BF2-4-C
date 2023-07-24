d6f6b61cb3a04fa9254b175d5b45b524ae3a9b20
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2023-07-24 13:12:51 +0900

    enemy die

--- Enemy.cpp
+++ Enemy.cpp
@@ -43,15 +43,7 @@ void Enemy::Update()
 	}
 	else if(StartFlg != 0)
 	{
-		HitStage();
-		HitFlg = HitEnemy();
-		HitPFlg = HitPlayer();
-		EnemyMove();
-	}
-	else
-	{
-		EnemyInit();
-		StartFlg = 0;
+		EnemyMove();		
 	}
 
 	//１秒たったらフレームカウント
@@ -67,10 +59,10 @@ void Enemy::Draw() const
 {
 #ifdef _DEBUG
 	DrawFormatString(50, 50, 0xffffff, "EnX:%f EnY:%f", enemy[2].x, enemy[2].y);
-	DrawFormatString(50, 70, 0xffffff, "Enflg:%d", enemy[0].flg);
-	DrawFormatString(50, 100, 0xffffff, "Px:%f", Px);
-	DrawFormatString(50, 130, 0xffffff, "Py:%f", Py);
-	DrawFormatString(50, 160, 0xffffff, "Hit:%d", HitPFlg);
+	DrawFormatString(50, 70, 0xffffff, "Enflg:%d", enemy[0].life);
+	DrawFormatString(50, 90, 0xffffff, "Enflg:%d", enemy[1].life);
+	DrawFormatString(50, 110, 0xffffff, "Enflg:%d", enemy[2].life);
+	DrawFormatString(130, 70, 0xffffff, "Hit:%d", HitPFlg);
 	
 
 	for (int i = 0; i < ENEMY_MAX; i++)
@@ -107,7 +99,7 @@ void Enemy::Draw() const
 	for (int i = 0; i < ENEMY_MAX; i++)
 	{
 		// 敵画像の表示
-		if (enemy[i].life != 0)
+		if (enemy[i].life != 6)
 		{
 			if (StartFlg == 0)
 			{
@@ -157,67 +149,77 @@ void Enemy::EnemyMove()
 {
 	for (int i = 0; i < ENEMY_MAX; i++)
 	{
-		// 敵のX座標範囲
-		if (enemy[i].x <= 0)
+		if (enemy[i].life != 0)
 		{
-			enemy[i].x = 640;
-		}
-		else if (enemy[i].x > 640)
-		{
-			enemy[i].x = 0;
-		}
+			HitStage(i);
+			HitFlg = HitEnemy(i);
+			HitPFlg = HitPlayer(i);
 
-		// 敵のY座標範囲
-		if (enemy[i].y <= -20)
-		{
-			enemy[i].y = enemy[i].y * 0.8f;
-		}
-		else if (enemy[i].y > 356)
-		{
-			enemy[i].speed = enemy[i].speed;
-			enemy[i].y = 356;
-		}
+			// 敵のX座標範囲
+			if (enemy[i].x <= 0)
+			{
+				enemy[i].x = 640;
+			}
+			else if (enemy[i].x > 640)
+			{
+				enemy[i].x = 0;
+			}
+
+			// 敵のY座標範囲
+			if (enemy[i].y <= -20)
+			{
+				enemy[i].y = enemy[i].y * 0.8f;
+			}
+			else if (enemy[i].y > 356)
+			{
+				enemy[i].speed = enemy[i].speed;
+				enemy[i].y = 356;
+			}
 
 
-		
-		if (Px >= enemy[i].x + 50)
-		{
-			// プレイヤーが敵より右にいるときは右に移動
-			// X座標加算
-			enemy[i].x += enemy[i].speed;
-			enemy[i].direction = 1;
+			if (Px >= enemy[i].x + 50)
+			{
+				// プレイヤーが敵より右にいるときは右に移動
+				// X座標加算
+				enemy[i].x += enemy[i].speed;
+				enemy[i].direction = 1;
 
+			}
+			else
+			{
+				// プレイヤーが敵より左にいるときは左に移動
+				// X座標減算
+				enemy[i].x -= enemy[i].speed;
+				enemy[i].direction = 0;
+
+			}
+
+
+			// プレイヤーが敵より上にいるときは浮上する
+			if (Py < enemy[i].y || enemy[i].ground == 1)
+			{
+				enemy[i].speed = 0.6f;
+
+				EnemyUp(i);
+
+				// Y座標減算
+				enemy[i].y -= enemy[i].speed;
+			}
+			else
+			{
+				EnemyDown(i);
+
+				// Y座標減算
+				enemy[i].y += enemy[i].speed;
+
+			}
+
+			enemy[i].ground = 0;
 		}
 		else
 		{
-			// プレイヤーが敵より左にいるときは左に移動
-			// X座標減算
-			enemy[i].x -= enemy[i].speed;
-			enemy[i].direction = 0;
-
+			EnemyDie(i);
 		}
-
-
-		// プレイヤーが敵より上にいるときは浮上する
-		if (Py < enemy[i].y || enemy[i].ground == 1)
-		{
-			enemy[i].speed = 0.6f;
-		
-			EnemyUp();
-
-			// Y座標減算
-			enemy[i].y -= enemy[i].speed;
-		}
-		else
-		{
-			EnemyDown();
-
-			// Y座標減算
-			enemy[i].y += enemy[i].speed;
-
-		}
-		
-		enemy[i].ground = 0;
 	}
 	
 }
@@ -277,150 +279,141 @@ void Enemy::StartMove()
 }
 
 // 敵の浮上モーション処理
-void Enemy::EnemyUp()
+void Enemy::EnemyUp(int e)
 {
-	for (int i = 0; i < ENEMY_MAX; i++)
+	
+	// パタパタ手を動かすモーション
+	if (FPScnt > 0 && FPScnt < 5 || FPScnt > 11 && FPScnt < 15 || FPScnt > 21 && FPScnt < 25)
 	{
-		// パタパタ手を動かすモーション
-		if (FPScnt > 0 && FPScnt < 5 || FPScnt > 11 && FPScnt < 15 || FPScnt > 21 && FPScnt < 25)
-		{
-			enemy[i].flg = 8;
-		}
-		else if (FPScnt > 31 && FPScnt < 35 || FPScnt > 41 && FPScnt < 45 || FPScnt > 51 && FPScnt < 55)
-		{
-			enemy[i].flg = 8;
-		}
-		else
-		{
-			enemy[i].flg = 9;
-		}
+		enemy[e].flg = 8;
 	}
+	else if (FPScnt > 31 && FPScnt < 35 || FPScnt > 41 && FPScnt < 45 || FPScnt > 51 && FPScnt < 55)
+	{
+		enemy[e].flg = 8;
+	}
+	else
+	{
+		enemy[e].flg = 9;
+	}
+	
 }
 
 // 敵の降下モーション処理
-void Enemy::EnemyDown()
+void Enemy::EnemyDown(int e)
 {
-	for (int i = 0; i < ENEMY_MAX; i++)
+	
+	// 降下モーション
+	if (FPScnt > 0 && FPScnt < 20)
 	{
-		// 降下モーション
-		if (FPScnt > 0 && FPScnt < 20)
-		{
-			enemy[i].flg = 11;
-		}
-		else if (FPScnt > 41  && FPScnt < 60 )
-		{
-			enemy[i].flg = 12;
-		}
-		else
-		{
-			enemy[i].flg = 10;
-
-			// 10 11 10 12 10 
-		}
+		enemy[e].flg = 11;
 	}
+	else if (FPScnt > 41  && FPScnt < 60 )
+	{
+		enemy[e].flg = 12;
+	}
+	else
+	{
+		enemy[e].flg = 10;
+
+		// 10 11 10 12 10 
+	}
+	
 }
 
 // 敵同士の当たり判定
-int Enemy::HitEnemy()
+int Enemy::HitEnemy(int e)
 {
+	EnXL[e] = enemy[e].x + 10.0f;
+	EnYL[e] = enemy[e].y + 12.0f;
+	EnXR[e] = EnXL[e] + 45.0f;
+	EnYR[e] = EnYL[e] + 53.0f;
 
-	for (int i = 0; i < ENEMY_MAX; i++)
+	if (e == 0)
 	{
-		EnXL[i] = enemy[i].x + 10.0f;
-		EnYL[i] = enemy[i].y + 12.0f;
-		EnXR[i] = EnXL[i] + 45.0f;
-		EnYR[i] = EnYL[i] + 53.0f;
-
-		if (i == 0)
+		if (EnXL[e] <= EnXR[e + 1] && EnYL[e] <= EnYR[e + 1] && EnXR[e] >= EnXL[e + 1] && EnYR[e] >= EnYL[e])
 		{
-			if (EnXL[i] <= EnXR[i + 1] && EnYL[i] <= EnYR[i + 1] && EnXR[i] >= EnXL[i + 1] && EnYR[i] >= EnYL[i])
-			{
-				enemy[i].x -= 10.0f;
-				return 1;
-			}
-		}
-		else if (i == 1)
-		{
-			if (EnXL[i] <= EnXR[i + 1] && EnYL[i] <= EnYR[i + 1] && EnXR[i] >= EnXL[i + 1] && EnYR[i] >= EnYL[i])
-			{
-				enemy[i].x -= 10.0f;
-				return 2;
-			}
-		}
-		else if (i == 2)
-		{
-			if (EnXL[i] <= EnXR[i - 2] && EnYL[i] <= EnYR[i - 2] && EnXR[i] >= EnXL[i - 2] && EnYR[i] >= EnYL[i])
-			{
-				enemy[i].x -= 10.0f;
-				return 3;
-			}
+			enemy[e].x -= 10.0f;
+			return 1;
 		}
 	}
-
+	else if (e == 1)
+	{
+		if (EnXL[e] <= EnXR[e + 1] && EnYL[e] <= EnYR[e + 1] && EnXR[e] >= EnXL[e + 1] && EnYR[e] >= EnYL[e])
+		{
+			enemy[e].x -= 10.0f;
+			return 2;
+		}
+	}
+	else if (e == 2)
+	{
+		if (EnXL[e] <= EnXR[e - 2] && EnYL[e] <= EnYR[e - 2] && EnXR[e] >= EnXL[e - 2] && EnYR[e] >= EnYL[e])
+		{
+			enemy[e].x -= 10.0f;
+			return 3;
+		}
+	}
+	
 	return 0;
 }
 
 // 敵とステージの当たり判定処理
-void Enemy::HitStage()
+void Enemy::HitStage(int e)
 {
 	int EnXL[ENEMY_MAX] = { 0 }, EnYL[ENEMY_MAX] = { 0 };//左上
 	int EnXR[ENEMY_MAX] = { 0 }, EnYR[ENEMY_MAX] = { 0 };//右下
 
-	for (int i = 0; i < ENEMY_MAX; i++)
-	{
-		EnXL[i] = (int)enemy[i].x + 10;
-		EnYL[i] = (int)enemy[i].y + 12;
-		EnXR[i] = EnXL[i] + 50;
-		EnYR[i] = EnYL[i] + 60;
-	}
+	
+	EnXL[e] = (int)enemy[e].x + 10;
+	EnYL[e] = (int)enemy[e].y + 12;
+	EnXR[e] = EnXL[e] + 50;
+	EnYR[e] = EnYL[e] + 60;
+	
 
-	for (int i = 0; i < ENEMY_MAX; i++)
-	{
-		if (EnXL[i] <= S_Ground_Left_XL && EnYR[i] >= S_Ground_Left_YU)
-		{//左下の台（側面）
-			enemy[i].ground = 1;
-			enemy[i].y -= 0.8f;//反発係数？
-		}
-		else if (EnXR[i] >= S_Ground_Right_XU && EnYR[i] >= S_Ground_Right_YU) 
-		{//右下の台（側面）
-			enemy[i].ground = 1;
-			enemy[i].y -= 0.8f;
-		}
-		else if (EnXR[i] >= S_Sky_Ground_0_XU && EnXR[i] <= S_Sky_Ground_0_XU && EnYR[i] >= S_Sky_Ground_0_YU  && EnYL[i] <= S_Sky_Ground_0_YL) 
-		{//上の台の左（側面）
-			enemy[i].ground = 1;
-			enemy[i].y -= 0.8f;
-		}
-		else if (EnXL[i] >= S_Sky_Ground_0_XL && EnXL[i] <= S_Sky_Ground_0_XL && EnYR[i] >= S_Sky_Ground_0_YU && EnYL[i] <= S_Sky_Ground_0_YL) 
-		{//上の台の右（側面）
-			enemy[i].ground = 1;
-			enemy[i].y -= 0.8f;
-		}
-		else if (EnYL[i] <= S_Sky_Ground_0_YL && EnYR[i] >= S_Sky_Ground_0_YU)
-		{//上の台（下辺）
-			if (EnXL[i] <= S_Sky_Ground_0_XL && EnXR[i] >= S_Sky_Ground_0_XU)
-			{
-				enemy[i].y -= 0.8f;
-				//if (VectorY >= 0)
-				//{//めり込まないようにするために加速度が０以上になると加速度に１足す
-				//	VectorY += 1.0f;
-				//}
-			}
-		}
-		else if (EnYL[i] <= 0)
-		{//画面上の当たり判定
-			enemy[i].ground = 1;
-			enemy[i].y -= 0.8f;
-			//if (VectorY >= 0) 
+	if (EnXL[e] <= S_Ground_Left_XL && EnYR[e] >= S_Ground_Left_YU)
+	{//左下の台（側面）
+		enemy[e].ground = 1;
+		enemy[e].y -= 0.8f;//反発係数？
+	}
+	else if (EnXR[e] >= S_Ground_Right_XU && EnYR[e] >= S_Ground_Right_YU) 
+	{//右下の台（側面）
+		enemy[e].ground = 1;
+		enemy[e].y -= 0.8f;
+	}
+	else if (EnXR[e] >= S_Sky_Ground_0_XU && EnXR[e] <= S_Sky_Ground_0_XU && EnYR[e] >= S_Sky_Ground_0_YU  && EnYL[e] <= S_Sky_Ground_0_YL) 
+	{//上の台の左（側面）
+		enemy[e].ground = 1;
+		enemy[e].y -= 0.8f;
+	}
+	else if (EnXL[e] >= S_Sky_Ground_0_XL && EnXL[e] <= S_Sky_Ground_0_XL && EnYR[e] >= S_Sky_Ground_0_YU && EnYL[e] <= S_Sky_Ground_0_YL) 
+	{//上の台の右（側面）
+		enemy[e].ground = 1;
+		enemy[e].y -= 0.8f;
+	}
+	else if (EnYL[e] <= S_Sky_Ground_0_YL && EnYR[e] >= S_Sky_Ground_0_YU)
+	{//上の台（下辺）
+		if (EnXL[e] <= S_Sky_Ground_0_XL && EnXR[e] >= S_Sky_Ground_0_XU)
+		{
+			enemy[e].y -= 0.8f;
+			//if (VectorY >= 0)
 			//{//めり込まないようにするために加速度が０以上になると加速度に１足す
 			//	VectorY += 1.0f;
 			//}
 		}
 	}
+	else if (EnYL[e] <= 0)
+	{//画面上の当たり判定
+		enemy[e].ground = 1;
+		enemy[e].y -= 0.8f;
+		//if (VectorY >= 0) 
+		//{//めり込まないようにするために加速度が０以上になると加速度に１足す
+		//	VectorY += 1.0f;
+		//}
+	}
+	
 }
 
 // 敵とプレイヤーの当たり判定
-int Enemy::HitPlayer()
+int Enemy::HitPlayer(int e)
 {
 	//プレイヤーの矩形の座標
 	float PXL, PYL;//左上
@@ -431,21 +424,29 @@ int Enemy::HitPlayer()
 	PXR = Px + 40;//右下X
 	PYR = Py+ 64;//右下Y
 
-	for (int i = 0; i < ENEMY_MAX; i++)
+	
+	EnXL[e] = enemy[e].x + 10.0f;
+	EnYL[e] = enemy[e].y + 12.0f;
+	EnXR[e] = EnXL[e] + 45.0f;
+	EnYR[e] = EnYL[e] + 53.0f;
+
+		
+	if (EnXL[e] <= PXR && EnYL[e] <= PYR && EnXR[e] >= PXL && EnYR[e] >= PYL)
 	{
-		EnXL[i] = enemy[i].x + 10.0f;
-		EnYL[i] = enemy[i].y + 12.0f;
-		EnXR[i] = EnXL[i] + 45.0f;
-		EnYR[i] = EnYL[i] + 53.0f;
-
-		if (EnXL[i] <= PXR && EnYL[i] <= PYR && EnXR[i] >= PXL && EnYR[i] >= PYL)
-		{
-			enemy[i].flg = 17;
-			return i;
-		}
+		enemy[e].flg = 17;
+		// 仮
+		enemy[e].life = 0;
+		return e;
 	}
-
+		
 	return 3;
 }
 
-
+// 敵の死亡モーション処理
+void Enemy::EnemyDie(int e)
+{
+	if(enemy[e].y <= 490.0f)
+	{
+		enemy[e].y += 0.6f;
+	}
+}
--- Enemy.h
+++ Enemy.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define ENEMY_X 200    // 敵[1]のX座標
-#define ENEMY_Y 185    // 敵[1]のY座標
-#define ENEMY_W 55     // 敵[1]の幅
-#define ENEMY_H 65     // 敵[1]の高さ
+#define ENEMY_X 200    // 敵[0]のX座標
+#define ENEMY_Y 185    // 敵[0]のY座標
+#define ENEMY_W 55     // 敵の幅
+#define ENEMY_H 65     // 敵の高さ
 #define ENEMY_MAX 3    // 敵の最大数
 
 class Enemy
@@ -16,8 +16,8 @@ private:
 	float Px, Py;      // プレイヤーのX座標、Y座標
 	int HitFlg;        // 敵同士の当たり判定用 
 	int HitPFlg;       // プレイヤーと敵の当たり判定用
-	float EnXL[ENEMY_MAX], EnYL[ENEMY_MAX]; //左上
-	float EnXR[ENEMY_MAX], EnYR[ENEMY_MAX]; //右下
+	float EnXL[ENEMY_MAX], EnYL[ENEMY_MAX]; //敵の左上座標
+	float EnXR[ENEMY_MAX], EnYR[ENEMY_MAX]; //敵の右下座標
 
 	// 敵のデータ(構造体)
 	struct ENEMY
@@ -27,7 +27,7 @@ private:
 		int state = 0;      // 敵の状態(0:ピンク 1:緑 2:黄色)
 		int life = 2;       // 敵のHP(0:死亡 1:パラシュート状態 2:風船1個状態)
 		int flg = 0;        // 敵の画像番号用
-		float speed = 0.6f; // 敵の移動速度
+		float speed = 0.3f; // 敵の移動速度
 		int direction = 0;  // 敵の向いてる向き(0:左 1:右)
 		int ground = 0;     // 地面に触れているか(0:触れてない 1:触れている)
 	};
@@ -41,11 +41,12 @@ public:
 	void EnemyInit();  // 敵の初期化処理
 	void EnemyMove();  // 敵の移動処理
 	void StartMove();  // 敵のスタート処理
-	void EnemyUp();    // 敵の浮上モーション処理
-	void EnemyDown();  // 敵の降下モーション処理
-	int HitEnemy();    // 敵同士の当たり判定
-	int HitPlayer();   // 敵とプレイヤーの当たり判定
-	void HitStage();   // 敵とステージの当たり判定
+	void EnemyUp(int e);    // 敵の浮上モーション処理
+	void EnemyDown(int e);  // 敵の降下モーション処理
+	int HitEnemy(int e);    // 敵同士の当たり判定
+	int HitPlayer(int e);   // 敵とプレイヤーの当たり判定
+	void HitStage(int e);   // 敵とステージの当たり判定
+	void EnemyDie(int e);   // 敵の死亡モーション処理
 
 	
 	//描画以外の更新を実装する
